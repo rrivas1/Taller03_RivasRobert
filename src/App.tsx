d9c70c4181fd1b938f74db3d09d3246a92f5ff9a
rrivas1 <rrivas1@cuc.edu.co>
2024-09-24 12:48:24 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import milogo from "../src/assets/img/perro-1.webp";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="Logo React" />
+        <h1>¡¡¡WOUFFF!!!</h1>
+        <p>Este es mi taller de react</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
